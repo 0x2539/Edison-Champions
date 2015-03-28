3265dd440e3c0f04a7076036cdd7ec1875025a00
Gabi Purcaru <gabi@purcaru.com>
2015-03-28 21:07:14 +0200

    added /users_nearby/ endpoint

--- django/server/main/urls.py
+++ django/server/main/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from .views import DingView, UserView, LevelView, UsersNearEdisonView
+from .views import UsersNearby
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'index'),
     url(r'^ding/$', DingView.as_view()),
     url(r'^user/$', UserView.as_view()),
     url(r'^users_near_edison/$', UsersNearEdisonView.as_view()),
+    url(r'^users_nearby/$', UsersNearby.as_view()),
     url(r'^level/$', LevelView.as_view()),
 )
