70f4ef2ddfae96053e982ccf0d323e2c3f1090ea
alexbuicescu <alexbuicescu@gmail.com>
2015-03-28 18:25:03 +0200

    structured android project

--- android/Finder/app/src/main/java/example/com/finder/Activities/MainActivity.java
+++ android/Finder/app/src/main/java/example/com/finder/Activities/MainActivity.java
@@ -1,9 +1,11 @@
-package example.com.finder;
+package example.com.finder.Activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import example.com.finder.R;
 
 
 public class MainActivity extends ActionBarActivity {
