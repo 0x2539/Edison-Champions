efe2e085a923d49eba87eb1cbdb9e5fe37873909
alexbuicescu <alexbuicescu@gmail.com>
2015-03-28 18:59:17 +0200

    created basic classes

---
+++ android/Finder/app/src/main/java/example/com/finder/Adapters/PeopleListViewAdapter.java
@@ -0,0 +1,90 @@
+package example.com.finder.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import example.com.finder.R;
+
+/**
+ * Created by Alexandru on 28-Mar-15.
+ */
+public class PeopleListViewAdapter extends BaseAdapter {
+    /**
+     * The inflater.
+     */
+    protected LayoutInflater inflater;
+
+    /**
+     * The items list.
+     */
+    protected List<String> items;
+
+    /**
+     * The context.
+     */
+    private Context context;
+
+    /**
+     * The Class ViewHolder.
+     */
+    private static class ViewHolder {
+        TextView theTitleTextView;
+        TextView theSubtitleTextView;
+    }
+
+    public PeopleListViewAdapter(Context context, List<String> items) {
+        this.context = context;
+        this.items = items;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setPeople(List<String> people)
+    {
+        items = people;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.people_item_layout, parent, false);
+
+            holder = new ViewHolder();
+
+//            holder.theTitleTextView = (TextView) convertView.findViewById(R.id.listview_adapter_title_textview);
+//            holder.theSubtitleTextView = (TextView) convertView.findViewById(R.id.listview_adapter_subtitle_textview);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+//        holder.theTitleTextView.setText(items.get(position));
+//        holder.theSubtitleTextView.setText("Item #" + position);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+}
---
+++ android/Finder/app/src/main/java/example/com/finder/Layouts/PeopleListLayout.java
@@ -0,0 +1,91 @@
+package example.com.finder.Layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import example.com.finder.Adapters.PeopleListViewAdapter;
+import example.com.finder.R;
+import example.com.finder.Utils.PeopleUtils;
+
+/**
+ * Created by Alexandru on 28-Mar-15.
+ */
+public class PeopleListLayout extends BaseFragLayout {
+
+    public interface OnPeopleListFragmentListener {
+        public void onPersonClicked(int position);
+    }
+
+    private Activity context;
+
+    private OnPeopleListFragmentListener listener;
+    private View view;
+    protected ListView peopleListView;
+
+    private TextView noPeopleTextView;
+
+    private PeopleListViewAdapter peopleListItemAdapter;
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        if (activity instanceof OnPeopleListFragmentListener)
+        {
+            listener = (OnPeopleListFragmentListener) activity;
+        } else
+        {
+            throw new ClassCastException(activity.toString() + " must implemenet OnPeopleListFragmentListener");
+        }
+        context = activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.frag_people, container, false);
+        initAll();
+
+        return view;
+    }
+
+    private void initAll()
+    {
+        try {
+            noPeopleTextView = (TextView) view.findViewById(R.id.frag_people_empty_text_view);
+            peopleListView = (ListView) view.findViewById(R.id.frag_people_list_view);
+            updateView();
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+
+    private void updateView()
+    {
+        if (peopleListItemAdapter == null)
+        {
+            peopleListItemAdapter = new PeopleListViewAdapter(context,
+                    PeopleUtils.getPeople());
+            peopleListView.setAdapter(peopleListItemAdapter);
+        } else
+        {
+            peopleListItemAdapter.setPeople(PeopleUtils.getPeople());
+        }
+
+        if(noPeopleTextView != null) {
+            if (PeopleUtils.getPeople().size() == 0) {
+                noPeopleTextView.setVisibility(View.VISIBLE);
+            } else {
+                noPeopleTextView.setVisibility(View.GONE);
+            }
+            peopleListItemAdapter.notifyDataSetChanged();
+        }
+    }
+}
